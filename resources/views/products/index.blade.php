af859cb9745af66b40dee899284fec557636b1c7
maurodirosa <maurodirosa19@gmail.com>
2023-08-16 22:40:03 +0200

    handle delete.

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Product</h1>
+    <div>
+        @if(session('success'))
+        <p>{{session('success')}}</p>
+        @endif
+    </div>
     <div>
         <table>
             <tr>
@@ -15,19 +22,28 @@
                 <th>Qty</th>
                 <th>Price</th>
                 <th>Description</th>
-                <th>Action</th>
+                <th>Edit</th>
+                <th>Delete</th>
             </tr>
             @foreach($products as $product)
-                <tr>
-                    <td>{{$product->id}}</td>
-                    <td>{{$product->name}}</td>
-                    <td>{{$product->qty}}</td>
-                    <td>{{$product->price}}</td>
-                    <td>{{$product->description}}</td>
-                    <td><a href="{{route('product.edit', ['product' => $product])}}">Edit</a></td>
-                </tr>
+            <tr>
+                <td>{{$product->id}}</td>
+                <td>{{$product->name}}</td>
+                <td>{{$product->qty}}</td>
+                <td>{{$product->price}}</td>
+                <td>{{$product->description}}</td>
+                <td><a href="{{route('product.edit', ['product' => $product])}}">Edit</a></td>
+                <td>
+                    <form action="{{route('product.destroy', ['product' => $product])}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">Delete</button>
+                    </form>
+                </td>
+            </tr>
             @endforeach
         </table>
     </div>
 </body>
+
 </html>
